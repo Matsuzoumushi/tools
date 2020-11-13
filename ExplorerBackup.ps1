1dfc997869ba8eec9e2db67fc5e9ec3bbb9ac89d
Vulgare <46861208+Vulgare@users.noreply.github.com>
2020-11-13 15:43:32 +0900

    Update ExplorerBackup.ps1

--- ExplorerBackup.ps1
+++ ExplorerBackup.ps1
@@ -1,4 +1,4 @@
-﻿#This code saves the path of the open explorer.
+#This code saves the path of the open explorer.
 #The Created batch file reopens saved explorer path.
 #Batch file will be created in $OutputFolder.
 
@@ -9,12 +9,12 @@ $Explorer = $shell.windows()|Where-Object{$_.fullname -like "*Explorer.EXE"}
 $Output = New-Item -path ($OutputFolder+"BackupExplorer_"+(get-date -Format "yyyyMMddHHmmss")+".bat")
 
 if ($Explorer.Count -eq 0){
-Write-Output "nothing"
+  Write-Output "nothing"
 }else{
-Write-Output "------------------saved path------------------"
-foreach($Fullpath in $Explorer){
-Add-Content -value ("explorer """+$Fullpath.locationURL + """") -path $Output
-Write-Output $Fullpath.locationURL
-} 
+  Write-Output "------------------saved path------------------"
+  foreach($Fullpath in $Explorer){
+    Add-Content -value ("explorer """+$Fullpath.locationURL + """") -path $Output
+    Write-Output $Fullpath.locationURL
+  } 
 }
 pause
